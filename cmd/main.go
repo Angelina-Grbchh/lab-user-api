24254dfb116cb276ee6f29fc874946532a2d6b7b
Angelina-Grbchh <gorbachangelina1810@gmail.com>
2025-05-09 16:50:45 +0300

    changes

--- cmd/main.go
+++ cmd/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
-    "lab-user-api/internal/handler"
-    "lab-user-api/store"
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 
-    "github.com/gorilla/mux"
+	"github.com/Angelina-Grbchh/lab-user-api/internal/handler"
+	"github.com/Angelina-Grbchh/lab-user-api/store"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    r := mux.NewRouter()
-    st := store.NewUserStore()
-    srv := handler.NewServer(st)
+	r := mux.NewRouter()
+	st := store.NewUserStore()
+	srv := handler.NewServer(st)
 
-    r.HandleFunc("/users", srv.CreateUser).Methods("POST")
-    r.HandleFunc("/users", srv.ListUsers).Methods("GET")
-    r.HandleFunc("/users/{id}", srv.GetUser).Methods("GET")
-    r.HandleFunc("/users/{id}", srv.UpdateUser).Methods("PUT")
-    r.HandleFunc("/users/{id}", srv.DeleteUser).Methods("DELETE")
+	r.HandleFunc("/users", srv.CreateUser).Methods("POST")
+	r.HandleFunc("/users", srv.ListUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", srv.GetUser).Methods("GET")
+	r.HandleFunc("/users/{id}", srv.UpdateUser).Methods("PUT")
+	r.HandleFunc("/users/{id}", srv.DeleteUser).Methods("DELETE")
 
-    log.Println("Server started on :8080")
-    http.ListenAndServe(":8080", r)
+	log.Println("Server started on :8080")
+	http.ListenAndServe(":8080", r)
 }
